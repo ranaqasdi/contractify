61073560bed3b96a1b76707174a5f0c66b748edc
rana qasid <rqa166@gmail.com>
2025-04-12 00:35:10 +0500

    25

--- app/Components/Footer.js
+++ app/Components/Footer.js
@@ -20,19 +20,19 @@ export default function Footer() {
                 {/* Center - Quick Links */}
                 <div>
                     <h3 className="text-lg font-semibold">Quick Links</h3>
-                    <ul className="mt-2 space-y-2 text-gray-400">
-                        <li><a href="/" className="hover:text-gray-300">Home</a></li>
-                        <li><a href="/about" className="hover:text-gray-300">About Us</a></li>
-                        <li><a href="/contact" className="hover:text-gray-300">Contact</a></li>
+                    <ul className="mt-2 space-y-2 text-gray-300">
+                        <li><a href="/" className="hover:text-gray-200">Home</a></li>
+                        <li><a href="/about" className="hover:text-gray-200">About Us</a></li>
+                        <li><a href="/contact" className="hover:text-gray-200">Contact</a></li>
                     </ul>
                 </div>
                 <div>
                     <h3 className="text-lg font-semibold">Top Templates</h3>
-                    <ul className="mt-2 space-y-2 text-gray-400">
-                        <li><a href="/forms/influencer-contract/1" className="hover:text-gray-300">Influencer Contract</a></li>
-                        <li><a href="/forms/resume/1" className="hover:text-gray-300">Resume Templates</a></li>
-                        <li><a href="/forms/nda-forms" className="hover:text-gray-300">NDA FORMS</a></li>
-                        <li><a href="/forms/lease/1" className="hover:text-gray-300">Property Rental Template</a></li>
+                    <ul className="mt-2 space-y-2 text-gray-300">
+                        <li><a href="/forms/influencer-contract/1" className="hover:text-gray-200">Influencer Contract</a></li>
+                        <li><a href="/forms/resume/1" className="hover:text-gray-200">Resume Templates</a></li>
+                        <li><a href="/forms/nda-forms" className="hover:text-gray-200">NDA FORMS</a></li>
+                        <li><a href="/forms/lease/1" className="hover:text-gray-200">Property Rental Template</a></li>
                     </ul>
                 </div>
 
@@ -42,15 +42,15 @@ export default function Footer() {
                     <div className="flex mt-2 space-x-4">
                         <a href="https://www.facebook.com/share/18oghwwZyE/?mibextid=wwXIfr" className="text-gray-200 hover:text-white text-2xl"><FaFacebook /></a>
                         <a href="https://www.instagram.com/usetemplik" className="text-gray-200 hover:text-white text-2xl"><FaInstagram /></a>
-                        <a href="#" className="text-gray-200 hover:text-white text-2xl"><FaTwitter /></a>
-                        <a href="#" className="text-gray-200 hover:text-white text-2xl"><FaLinkedin /></a>
+                        <a href="#" className="text-gray-100 hover:text-white text-2xl"><FaTwitter /></a>
+                        <a href="#" className="text-gray-100 hover:text-white text-2xl"><FaLinkedin /></a>
                     </div>
                 </div>
 
             </div>
 
             {/* Bottom - Copyright */}
-            <div className="mt-14 text-center text-purple-300 border-t border-gray-700 py-5 ">
+            <div className="mt-14 text-center text-purple-200 border-t border-gray-500 py-5 ">
                 <p className="leading-none">© {new Date().getFullYear()} Templik. All rights reserved | Developed By <a href="https://xtreamstudios.com/" target="_blank">Xtream Studios</a>.</p>
             </div>
         </footer>
--- app/Components/Header.js
+++ app/Components/Header.js
@@ -21,14 +21,15 @@ export default function Header() {
         </nav>
 
         <button className="md:hidden" onClick={() => setIsOpen(!isOpen)}>
-          {isOpen ? <X className="w-6 h-6" /> : <Menu className="w-6 h-6" />}
+          {isOpen ? <X className="w-6 h-6 text-gray-900" /> : <Menu className="w-6 h-6 text-gray-900" />}
         </button>
       </div>
       
       {isOpen && (
         <nav className="md:hidden bg-white border-t p-4 space-y-4 shadow-md">
+          <Link href="/" className="block text-gray-600 hover:text-gray-900">Home</Link>
           <Link href="/about" className="block text-gray-600 hover:text-gray-900">About</Link>
-          {/* <Link href="/services" className="block text-gray-600 hover:text-gray-900">Services</Link> */}
+          <Link href="/forms" className="block text-gray-600 hover:text-gray-900">Create New Form</Link>
           <Link href="/contact" className="block text-gray-600 hover:text-gray-900">Contact</Link>
         </nav>
       )}
